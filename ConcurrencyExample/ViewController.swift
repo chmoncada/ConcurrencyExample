a27b554b0652e23493c152794c850cd05fdb416b
Charles Moncada <charles.moncada@emoshiapps.com>
2018-10-01 17:32:00 -0300

    Initial Commit

---
+++ ConcurrencyExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConcurrencyExample
+//
+//  Created by Charles Moncada on 01/10/18.
+//  Copyright © 2018 Charles Moncada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
