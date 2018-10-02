11b6fcf6f1fc4c8703b988b0843bc570654b21a5
Charles Moncada <charles.moncada@emoshiapps.com>
2018-10-02 13:06:20 -0300

    DispatchGroup use

--- ConcurrencyExample/ViewController.swift
+++ ConcurrencyExample/ViewController.swift
@@ -92,47 +92,69 @@ class ViewController: UIViewController {
 //		let myConcurrentQueue = DispatchQueue(label: "io.keepcoding.concurrent", attributes: .concurrent)
 
 		let myConcurrentQueue = DispatchQueue.global(qos: .userInitiated)
+
+		let group = DispatchGroup()
+
+		group.enter()
 		myConcurrentQueue.async {
 			let data1 = try! Data(contentsOf: URL(string: imageURL01)!)
 
 			DispatchQueue.main.async { [weak self] in
 				self?.image01.image = UIImage(data: data1)
+				group.leave()
 			}
 		}
 
+		group.enter()
 		myConcurrentQueue.async {
 			let data2 = try! Data(contentsOf: URL(string: imageURL02)!)
 			DispatchQueue.main.async { [weak self] in
 				guard let `self` = self else { return }
 				self.image02.image = UIImage(data: data2)
+				group.leave()
 			}
 		}
+
+		group.enter()
 		myConcurrentQueue.async {
 			let data3 = try! Data(contentsOf: URL(string: imageURL03)!)
 			DispatchQueue.main.async {
 				self.image03.image = UIImage(data: data3)
+				group.leave()
 			}
 		}
 
+		group.enter()
 		myConcurrentQueue.async {
 			let data4 = try! Data(contentsOf: URL(string: imageURL04)!)
 			DispatchQueue.main.async {
 				self.image04.image = UIImage(data: data4)
+				group.leave()
 			}
 		}
+
+		group.enter()
 		myConcurrentQueue.async {
 			let data5 = try! Data(contentsOf: URL(string: imageURL05)!)
 			DispatchQueue.main.async {
 				self.image05.image = UIImage(data: data5)
+				group.leave()
 			}
 		}
 
+		group.enter()
 		myConcurrentQueue.async {
 			let data6 = try! Data(contentsOf: URL(string: imageURL06)!)
 			DispatchQueue.main.async {
 				self.image06.image = UIImage(data: data6)
+				group.leave()
 			}
 		}
+
+		group.notify(queue: DispatchQueue.main) { [weak self] in
+			self?.syncFilterButton.isEnabled = true
+		}
+
 	}
 
 	
