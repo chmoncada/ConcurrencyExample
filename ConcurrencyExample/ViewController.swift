41f363ad66c82d9abef49cff0ac46734ba8505a0
Charles Moncada <charles.moncada@emoshiapps.com>
2018-10-02 13:01:42 -0300

    Serial Queue

--- ConcurrencyExample/ViewController.swift
+++ ConcurrencyExample/ViewController.swift
@@ -43,6 +43,48 @@ class ViewController: UIViewController {
 
 
 	@IBAction func privateQueueDownload(_ sender: Any) {
+		let mySerialQueue = DispatchQueue(label: "io.keepcoding.serial")
+
+		mySerialQueue.async {
+			let data1 = try! Data(contentsOf: URL(string: imageURL01)!)
+
+			DispatchQueue.main.async { [weak self] in
+				self?.image01.image = UIImage(data: data1)
+			}
+		}
+
+		mySerialQueue.async {
+			let data2 = try! Data(contentsOf: URL(string: imageURL02)!)
+			DispatchQueue.main.async { [weak self] in
+				guard let `self` = self else { return }
+				self.image02.image = UIImage(data: data2)
+			}
+		}
+
+		mySerialQueue.async {
+			let data3 = try! Data(contentsOf: URL(string: imageURL03)!)
+			DispatchQueue.main.async {
+				self.image03.image = UIImage(data: data3)
+			}
+		}
+		mySerialQueue.async {
+			let data4 = try! Data(contentsOf: URL(string: imageURL04)!)
+			DispatchQueue.main.async {
+				self.image04.image = UIImage(data: data4)
+			}
+		}
+		mySerialQueue.async {
+			let data5 = try! Data(contentsOf: URL(string: imageURL05)!)
+			DispatchQueue.main.async {
+				self.image05.image = UIImage(data: data5)
+			}
+		}
+		mySerialQueue.async {
+			let data6 = try! Data(contentsOf: URL(string: imageURL06)!)
+			DispatchQueue.main.async {
+				self.image06.image = UIImage(data: data6)
+			}
+		}
 	}
 
 	@IBAction func privateQueueConcurrentDownload(_ sender: Any) {
